473b555965729860234b8f697f7da98fa9021ddb
objelisks <objelisks@gmail.com>
2022-05-18 21:19:54 -0700

    add blog

--- blog/blog.mjs
+++ blog/blog.mjs
@@ -3,40 +3,28 @@ import Koa from 'koa'
 import serve from 'koa-static'
 import Router from '@koa/router'
 
-const themeButton = html`
-<section>
-  <button id="theme-switcher">switch theme</button>
-</section>
-`
-
-const train = html`
-<aside class="train" data-rotate="rotate(120deg)"><div class="stack"></div><div class="stack"></div><div class="stack"></div><div class="stack"></div></aside>
-`
-
 const body = html`
 <!doctype html>
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/common/styles.css" type="text/css" rel="stylesheet">
-    <script src="./index.js" type="module"></script>
+    <script src="./blog.js" type="module"></script>
   </head>
   <body>
-    <h1>welcome to objelisks space</h1>
-    ${themeButton}
-    ${train}
+    <h1>blog zone</h1>
   </body>
 </html>
 `
 
-const index = async (ctx, next) => {
+const blog = async (ctx, next) => {
   ctx.response.body = body.toString()
 }
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/', index)
+router.get('/', blog)
 app.use(router.routes())
 app.use(serve('./index'))
 
--- server.mjs
+++ server.mjs
@@ -1,15 +1,13 @@
 import Koa from 'koa'
 import serve from 'koa-static'
-import Router from '@koa/router'
 import mount from 'koa-mount'
 import index from './index/index.mjs'
+import blog from './blog/blog.mjs'
 
 const app = new Koa()
-const router = new Router()
 
-router.get('/', index)
-app.use(mount('/', router.routes()))
-app.use(mount('/', serve('./index')))
+app.use(mount('/', index))
+app.use(mount('/blog', blog))
 
 app.use(mount('/common', serve('./common')))
 
