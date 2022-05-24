8387efc97d06beb863785fcd9d845554e01d5dbd
objelisks <objelisks@gmail.com>
2022-05-24 12:39:00 -0700

    about page

---
+++ misc/misc.js
@@ -0,0 +1,9 @@
+const ready = (callback) => document.addEventListener('DOMContentLoaded', callback)
+const on = (selector, eventName, callback) => document.querySelector(selector).addEventListener(eventName, callback)
+
+ready(() => {
+  document.getElementById('page-size').innerText = `${performance
+    .getEntries()
+    .reduce((acc, resource) => {console.log(resource); return acc + resource.transferSize}, 0)/1000} kb`
+  document.getElementById('page-time').innerText = `${performance.now()} ms`
+})
--- misc/misc.mjs
+++ misc/misc.mjs
@@ -43,11 +43,28 @@ const columbo = html`
 </article>
 `
 
+const about = html`
+<article id="about">
+  <h2>about this web site</h2>
+  <p>
+    palettes:
+    <ul>  
+      <li><a href="https://lospec.com/palette-list/curiosities">curiosities</a></li>
+      <li><a href="https://lospec.com/palette-list/snail-village">snail village</a></li>
+      <li><a href="https://lospec.com/palette-list/blue-newspaper">blue newspaper</a></li>
+    </ul>
+  </p>
+  <p>sustainably powered by <a href="https://greenhost.net/">dutch windmills</a>.</p>
+  <p>total page size: <span id="page-size">?? KB</span>. loaded in <span id="page-time">?? ms</span>.</p>
+</article>
+`
+
 const body = html`
 <!doctype html>
 <html>
   <head>
     ${head}
+    <script src="./misc/misc.js" type="module"></script>
     <script>//:3</script>
   </head>
   <body>
@@ -55,23 +72,25 @@ const body = html`
     ${nav}
     <section>
       <a href="#columbo">columbo</a>
+      <a href="#about">about site</a>
     </section>
     <section>
       ${raw(columbo)}
+      ${raw(about)}
     </section>
   </body>
 </html>
 `
 
-const link = async (ctx, next) => {
+const misc = async (ctx, next) => {
   ctx.response.body = body.toString()
 }
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/', link)
+router.get('/', misc)
 app.use(router.routes())
-app.use(serve('./link'))
+app.use(serve('./misc'))
 
 export default app
