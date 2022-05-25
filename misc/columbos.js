3079d6b3ad15bcf6c7eb7ef6cfb3fe84e96af27b
objelisks <objelisks@gmail.com>
2022-05-25 07:57:03 -0700

    update

---
+++ misc/columbos.js
@@ -0,0 +1,32 @@
+
+const columbos = [
+  {ep:'S1E08', title: 'A Blueprint for Murder',
+  notes: `first horse sighting
+  multiple excellent lines:
+  you're very much like an arachnid
+  we could've used you at the alamo you've got guts
+  the omnipresent constable`},
+  {ep:'S1E07', title: 'Short Fuse',
+  notes: `the lil carts, i like the villain a lot`},
+  {ep:'S1E06', title: 'Lady in Waiting',
+  notes: `got steve martin in it`},
+  {ep:'S2E02', title: 'The Greenhouse Jungle',
+  notes: `ok i do like the new detective a lil bit, it fully emphasizes how columbo is his own thing and that the cops are just as useless in this universe`},
+  {ep:'S2E01', title: 'Etude in Black',
+  notes: `don't like the thing they do in s2 where they preview stuff that happens later`},
+  {ep:'S1E01', title: 'Ransom for a Dead Man',
+  notes: ``},
+  {ep:'S1E00', title: 'Prescription Murder',
+  notes: ``},
+  {ep:'S1E02', title: 'Murder by the Book',
+  notes: `the villain isn't charismatic enough, he's just an asshole`},
+  {ep:'S1E03', title: 'Death Lends a Hand',
+  notes: ``},
+  {ep:'S1E04', title: 'Dead Weight',
+  notes: `too much military deference`},
+  {ep:'S2E03', title: 'The Most Crucial Game',
+  notes: `lol establishing shot on the los angeles MEMORIAL coliseum with spooky music
+  using an ice cube as a weapon is great, but doesn't come up much in the ep`},
+]
+
+export default columbos
--- misc/misc.mjs
+++ misc/misc.mjs
@@ -4,33 +4,7 @@ import Koa from 'koa'
 import serve from 'koa-static'
 import Router from '@koa/router'
 import {head, nav} from '../common.mjs'
-
-const columbos = [
-  {ep:'S1E08', title: 'A Blueprint for Murder',
-  notes: `first horse sighting
-  multiple excellent lines:
-  you're very much like an arachnid
-  we could've used you at the alamo you've got guts
-  the omnipresent constable`},
-  {ep:'S1E07', title: 'Short Fuse',
-  notes: `the lil carts, i like the villain a lot`},
-  {ep:'S1E06', title: 'Lady in Waiting',
-  notes: `got steve martin in it`},
-  {ep:'S2E02', title: 'The Greenhouse Jungle',
-  notes: `ok i do like the new detective a lil bit, it fully emphasizes how columbo is his own thing and that the cops are just as useless in this universe`},
-  {ep:'S2E01', title: 'Etude in Black',
-  notes: `don't like the thing they do in s2 where they preview stuff that happens later`},
-  {ep:'S1E01', title: 'Ransom for a Dead Man',
-  notes: ``},
-  {ep:'S1E00', title: 'Prescription Murder',
-  notes: ``},
-  {ep:'S1E02', title: 'Murder by the Book',
-  notes: `the villain isn't charismatic enough, he's just an asshole`},
-  {ep:'S1E03', title: 'Death Lends a Hand',
-  notes: ``},
-  {ep:'S1E04', title: 'Dead Weight',
-  notes: `too much military deference`},
-]
+import columbos from './columbos.js'
 
 const columbo = html`
 <article id="columbo">
