267d3f39f9352499ea4779cc6c28925eecff6415
objelisks <objelisks@gmail.com>
2022-11-07 12:28:28 -0800

    update columbos

--- misc/columbos.mjs
+++ misc/columbos.mjs
@@ -18,11 +18,18 @@ const columbos = [
   notes: `got steve martin in it`},
   {ep:'S2E02', title: 'The Greenhouse Jungle',
   notes: `ok i do like the new detective a lil bit, it fully emphasizes how columbo is his own thing and that the cops are just as useless in this universe`},
+  {ep:'S2E08', title: 'Double Shock',
+  notes: `The twins gambit is fun.
+  columbo does an arms crossy thing in the theater thats very funny to me.`},
   {ep:'S2E01', title: 'Etude in Black',
   notes: `don't like the thing they do in s2 where they preview stuff that happens later`},
   {ep:'S2E03', title: 'The Most Crucial Game',
   notes: `lol establishing shot on the los angeles MEMORIAL coliseum with spooky music
   using an ice cube as a weapon is great, but doesn't come up much in the ep`},
+  {ep:'S2E07', title: 'Most Dangerous Match',
+  notes: ``},
+  {ep:'S3E01', title: 'Lovely but Lethal',
+  notes: `theres still poison oak in california..`},
   {ep:'S1E01', title: 'Ransom for a Dead Man',
   notes: ``},
   {ep:'S1E00', title: 'Prescription Murder',
